bba001a97cd0819e1191b1312c217a7b3ec1d22c
Virginia Ramos <virginia.rare@gmail.com>
2023-07-22 23:44:43 -0500

    Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Graph from './components/Graph';
 
 function App() {
   return (
     <div className="App">
-      <h1>Money Tracker</h1>
+      <div className= "container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded"> "Money Tracker"</h1>
+      
+        {/* grid columns */}
+        <div className="grid md:grid-cols2 gap-4">
+          {/* Chart */}
+          <Graph></Graph>
+          {/* Form */}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Graph.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Graph() {
+  return (
+    <div className='flex justify-content max-w-xs mx-auto'>
+      <div className='item'>
+        <div className='chart relative'>
+            Chart
+        </div>
+
+        <div className='flex flex-col py-10 gap-4'>
+            {/*Lables*/}
+        </div>
+      </div>
+    </div>
+  )
+}
