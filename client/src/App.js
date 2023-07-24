9f8553a03a3302d0656aaa08776aa6ffd168b12b
Virginia Ramos <virginia.rare@gmail.com>
2023-07-24 09:51:34 -0500

    Updating form colum

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <div className= "container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
-        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded"> "Money Tracker"</h1>
+        <h1 className="text-4xl py-8 mb-10 bg-blue-900 text-white rounded"> "Money Tracker"</h1>
       
-        {/* grid columns */}
-        <div className="grid md:grid-cols2 gap-4">
+      {/* grid columns */}
+      <div className="grid md:grid-cols-2 gap-4">
           {/* Chart */}
           <Graph></Graph>
           {/* Form */}
           <Form></Form>
-        </div>
       </div>
     </div>
+  </div>
   );
 }
 
