c295ee6c43dcb1dfe31c57863e19d789d642e433
Virginia Ramos <virginia.rare@gmail.com>
2023-07-25 06:57:18 -0500

    Up to frond end done

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -30,7 +30,7 @@ function Transaction({category}){
     if(!category) return null;
     return (
         <div className="item flex justify-center bg-gray-50 py-2 rounded-r" style={{ borderRight : `8px solid ${category.color ??  "#e5e5e5"}`}}>
-            <button className='px-3'><box-icon size="15px" color={category.color ??  "#e5e5e5"} name="trash"></box-icon></button>            
+            <button className='px-3'><box-icon size="20px" color={category.color ??  "#e5e5e5"} name="x-circle"></box-icon></button>            
             <span className='block w-full'>{category.name ?? ''}</span>
         </div>
     )
