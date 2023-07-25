d3d4278fb99a720f05fdab5b93c3db8a4764baa8
Virginia Ramos <virginia.rare@gmail.com>
2023-07-24 22:19:46 -0500

    List for transactions history

---
+++ client/src/components/List.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import 'boxicons'
+
+const obj = [
+    {
+    name:"Expense",
+    color: 'rgb(255, 99, 132)',
+},
+{
+    name:"Income",
+    color: 'rgb(54, 162, 235)',
+},
+{
+    name:"Investment",
+    color: 'rgb(255, 205, 86)',
+}
+]
+
+export default function List() {
+  return (
+    <div className= "flex flex-col py-6 gap-3">
+        <h1 className= 'py-4 font-bold text-xl'>Transaction History</h1>
+        {obj.map((v,i) => <Transaction key={i} category={v}></Transaction>)}
+        
+    </div>
+  )
+}
+
+function Transaction({category}){
+    if(!category) return null;
+    return (
+        <div className="item flex justify-center bg-gray-50 py-2 rounded-r" style={{ borderRight : `8px solid ${category.color ??  "#e5e5e5"}`}}>
+            <button className='px-3'><box-icon size="15px" color={category.color ??  "#e5e5e5"} name="trash"></box-icon></button>            
+            <span className='block w-full'>{category.name ?? ''}</span>
+        </div>
+    )
+}
