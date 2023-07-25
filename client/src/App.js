9ecda20bff74ca64606e5fbc08227cd75d574505
Virginia Ramos <virginia.rare@gmail.com>
2023-07-25 07:18:18 -0500

    Back end ready

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-custom">
       <div className= "container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
         <h1 className="text-4xl py-8 mb-10 bg-blue-900 text-white rounded"> "Money Tracker"</h1>
       
