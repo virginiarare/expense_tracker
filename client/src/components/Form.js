81c9a7b47568f9059ee63abfe5c6914c22f123fa
Virginia Ramos <virginia.rare@gmail.com>
2023-07-24 21:44:10 -0500

    Submit data

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,21 +3,28 @@ import { useForm } from 'react-hook-form';
 
 
 export default function Form() {
+    const {register, handleSubmit, resetField} = useForm();
+
+    const onSubmit =(data) =>{
+        console.log(data)
+    }
+
+
   return (
     <div className="form max-w-sm-auto w-96">
         <h1 className= 'font-bold pb-4 text-xl'>Transaction</h1>
-        <form id='form'>
+        <form id='form' onSubmit={handleSubmit(onSubmit)}>
             <div className='grid gap-4'>
                 <div className='input-group'>
-                    <input type='text' placeholder='Salary, House Rent, SIP' className='form-input'/>
+                    <input type='text'{...register('name')} placeholder='Salary, House Rent, SIP' className='form-input'/>
                 </div>
-                <select className='form-input'>
+                <select className='form-input' {...register('type')}>
                     <option value="Income" defaultValue>Income</option>
                     <option value="Expense">Expense</option>
                     <option value="Investment">Investment</option>
                 </select>
                 <div className="input-group">
-                    <input type="text" placeholder='Amount' className='form-input' />
+                    <input type="text" {...register('amount')} placeholder='Amount' className='form-input' />
                 </div>
                 <div className="submit-btn">
                     <button className='border py-2 text-white bg-blue-900 w-full'>Make Transaction</button>
