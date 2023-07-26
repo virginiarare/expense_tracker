cd0a06f47490427851abbbdc675e11d5d14ffff5
Virginia Ramos <virginia.rare@gmail.com>
2023-07-26 18:42:39 -0500

    changing background app

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App bg-blue-custom" style={{ backgroundImage: "url('https://media.istockphoto.com/id/91827912/photo/painted-blue-brick-wall-background.jpg?s=612x612&w=0&k=20&c=ShtIyOVrKkGHLMXJTXmpOIZhtE9untTRCBgJdgQRMn8=')",backgroundSize: 'cover' }}>
+    <div className="App bg-blue-custom" style={{ backgroundImage: "url('https://img.freepik.com/premium-vector/blue-night-brick-wall-background_608189-366.jpg')",backgroundSize: 'cover' }}>
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
         <h1 className="text-4xl py-8 mb-10 bg-blue-500 bg-opacity-75 text-white rounded w-full money-tracker-font"> M O N E Y &nbsp; T R A C K E R</h1>
         
