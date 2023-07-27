0aec8ede0035499566e8cd26005b95a82b0fa614
Virginia Ramos <virginia.rare@gmail.com>
2023-07-27 09:00:19 -0500

    get labels endpoint working

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App bg-blue-custom" style={{ backgroundImage: "url('https://img.freepik.com/premium-vector/blue-night-brick-wall-background_608189-366.jpg')",backgroundSize: 'cover' }}>
+    <div className="App bg-blue-custom" style={{ backgroundImage: "url('https://img.freepik.com/premium-vector/blue-night-brick-wall-background_608189-366.jpg')",backgroundSize: 'cover', minHeight: '100vh'  }}>
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
         <h1 className="text-4xl py-8 mb-10 bg-blue-500 bg-opacity-75 text-white rounded w-full money-tracker-font"> M O N E Y &nbsp; T R A C K E R</h1>
         
