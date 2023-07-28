ea5202b9d7006062d1552b342c2a1b0eb7e2e583
Virginia Ramos <virginia.rare@gmail.com>
2023-07-28 10:14:59 -0500

    final changes

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -34,7 +34,7 @@ export default function Form() {
                     <input type="text" {...register('amount')} placeholder='Amount' className='form-input' />
                 </div>
                 <div className="submit-btn">
-                    <button className='border py-2 text-white bg-blue-500 w-full'>Make Transaction</button>
+                    <button className='border py-2 text-white bg-blue-500 w-full'>Add Transaction</button>
                 </div>
             </div>
         </form>
